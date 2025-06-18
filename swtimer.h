728144ac13de0c661dfb1151867ee87826bb4101
anyZhiJian <937279457@qq.com>
2025-06-18 22:38:15 +0800

    update250618

---
+++ swtimer.h
@@ -0,0 +1,32 @@
+#ifndef __SWTIMER_H
+#define __SWTIMER_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define swtim_malloc(size) malloc(size)
+#define swtim_free(ptr) free(ptr)
+#define SWTIM_MAX_TICKS ((uint32_t)-1 >> 1)
+
+typedef struct swtim_t swtim_t;
+typedef void (*swtim_cb_t)(swtim_t *timer, void *user_data);
+
+swtim_t *swtim_create(uint32_t period_ticks, swtim_cb_t cb, void *user_data);
+void swtim_delete(swtim_t *timer);
+void swtim_create_init(swtim_t *timer, uint32_t period_ticks, swtim_cb_t cb, void *user_data);
+void swtim_start(swtim_t *timer);
+void swtim_stop(swtim_t *timer);
+void swtim_sys_inc(uint16_t ticks);
+void swtim_handler(swtim_t *timer);
+void swtim_register_callback(swtim_t *timer, swtim_cb_t cb, void *user_data);
+void swtim_set_period(swtim_t *timer, uint32_t period_ticks);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
